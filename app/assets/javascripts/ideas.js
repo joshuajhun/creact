f2f85346d0c08fb647ab8b7c9c37ce079642a577
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-10-13 09:07:39 -0600

    render all ideas on page load

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -1,27 +1,33 @@
 $(document).ready(function() {
-  renderHeader();
-  renderSectionTitle();
+  renderHeaderLinks();
+  loadAllIdeas();
 });
 
-
-function renderHeader() {
+function renderHeaderLinks() {
   return $('.links').append(
     '<img class="js-logo" src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" />'
     + '<a href="#" class="not-a-link">Ideas</a><a id="link" href="#">All Ideas</a>'
   )
 }
 
-function renderSectionTitle() {
-  var title = $('a#link').on('click', function(event) {
-    $('.section-title').empty();
-    event.preventDefault();
-
-    var clicked = $(this).text();
-
-    $('.section-title').append(
-      '<h1>' + clicked + '</h1>'
-    )
+function loadAllIdeas() {
+  $.ajax({
+    type: "GET",
+    url: "/api/v1/ideas.json",
+    success: function(response) {
+      response.map(function(element) {
+        appendIdeaToDom(element);
+      });
+    }
   });
 }
 
-
+function appendIdeaToDom(idea) {
+  $('.all-ideas').prepend(
+    '<div class="idea">'
+    + '<h2>' + idea.title + '</h2>'
+    + '<p>' + idea.body + '</p>'
+    + '<p><b>Quality:</b> ' + idea.quality + '</p>'
+    + '</div>'
+  )
+}
