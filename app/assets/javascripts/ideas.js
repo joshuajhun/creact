5809ebbc27b107e08182e8a54a4be76e363639aa
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-10-14 13:26:46 -0600

    A way too big commit
    * extract ajax calls to an ideaService
    * change id's to classes since they were repeated on the DOM
    * start refactoring bind events to create them all at once

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
   renderHeaderLinks();
-  loadAllIdeas();
+  loadAllIdeas(function(ideas) {
+    renderIdeas(ideas);
+    bindEvents();
+  });
   submitNewIdea();
   deleteIdea();
 });
@@ -12,47 +15,56 @@ function renderHeaderLinks() {
   )
 }
 
-function loadAllIdeas() {
-  $.ajax({
-    type: 'GET',
-    url: '/api/v1/ideas.json',
-    success: function(response) {
-      response.map(function(element) {
-        appendIdeaToDom(element);
-      });
-    }
+function loadAllIdeas(callback) {
+  ideaService.index(function(response) {
+      callback(response);
+  });
+}
+
+function renderIdeas(ideas) {
+  ideas.map(function(idea) {
+    appendIdeaToDom(idea);
   });
 }
 
 function appendIdeaToDom(idea) {
   var newIdea = $('<div class="idea" data-id="' + idea.id + '">' +
-                    '<h2>' + idea.title + '</h2>' +
-                    '<p>' + truncateBody(idea.body) + '</p>' +
-                    '<p id="quality"><b>Quality:</b> ' + idea.quality + '</p>' +
+                    '<h2 id="title-' + idea.id + '">' + idea.title + '</h2>' +
+                    '<p id="body-' + idea.id + '">' + truncateBody(idea.body) + '</p>' +
+                    '<p class="quality"><b>Quality:</b> ' + idea.quality + '</p>' +
                     '<div class="quality-change">' +
-                      '<label id="quality-options"><input type="radio" name="radio-button' + idea.id + '" value="Swill">  Swill</label>' +
-                      '<label id="quality-options"><input type="radio" name="radio-button' + idea.id + '" value="Plausible">  Plausible</label>' +
-                      '<label id="quality-options"><input type="radio" name="radio-button' + idea.id + '" value="Genius">  Genius</label>' +
+                      '<label class="quality-options"><input type="radio" name="radio-button' + idea.id + '" value="Swill">  Swill</label>' +
+                      '<label class="quality-options"><input type="radio" name="radio-button' + idea.id + '" value="Plausible">  Plausible</label>' +
+                      '<label class="quality-options"><input type="radio" name="radio-button' + idea.id + '" value="Genius">  Genius</label>' +
                     '</div></p>' +
-                    '<a id="full-body" class="btn btn-sm btn-default">Full</a>' +
-                    '<a id="remove-idea" class="btn btn-sm btn-default">Remove</a>' +
-                    '<a id="edit-idea-' + idea.id + '" class="btn btn-sm btn-default">Edit</a>' +
-                    '<div class="row hidden" id="edit-form-' + idea.id + '">' +
+                    '<a class="full-body btn btn-sm btn-default">Full</a>' +
+                    '<a class="remove-idea btn btn-sm btn-default">Remove</a>' +
+                    '<a class="edit-idea-' + idea.id + ' btn btn-sm btn-default">Edit</a>' +
+                    '<div class="row" style="display: none;" id="edit-form-' + idea.id + '">' +
                       '<div class="col-md-6">' +
                         '<div class="idea-form form-group">' +
                           '<label>Title</label>' +
-                            '<input class="form-control" type="text" id="idea-title" value="' + idea.title + '">' +
+                            '<input class="form-control" type="text" id="idea-title-' + idea.id + '" value="' + idea.title + '">' +
                           '<label>Content</label>' +
-                          '<input class="form-control" type="textfield" id="idea-body" value="' + idea.body + '">' +
+                          '<input class="form-control" type="textfield" id="idea-body-' + idea.id + '" value="' + idea.body + '">' +
                         '</div>' +
-                        '<input class="btn btn-default pull-right" id="submit-idea" type="button" name="submit" value="Create Idea">' +
+                        '<input class="btn btn-default pull-right" id="submit-idea-' + idea.id + '" type="button" name="submit" value="Create Idea">' +
                       '</div>' +
                     '</div>' +
                   '</div>');
 
-  addListenerForQualityChange(newIdea);
   addListenerForEdit(newIdea);
+  addListenerToSubmitEdits(newIdea);
   $('.ideas').prepend(newIdea);
+}
+
+function bindEvents() {
+  $('.ideas').on('click', '.quality-change input', function() {
+    onQualityChange.call(this, $(this).closest('.idea'));
+  })
+
+  // TODO: extract addListenerForEdit and
+  //               addListenerToSubmitEdits
 }
 
 function truncateBody(body) {
@@ -63,32 +75,51 @@ function truncateBody(body) {
   return body.slice(0, 100) + '...';
 }
 
-function addListenerForQualityChange(idea) {
-  idea.find('.quality-change input').on('click', function() {
-    var quality = $(this).val();
+function onQualityChange(idea) {
+  var quality = $(this).val();
+  var data = {idea: { quality: quality } }
 
-    $.ajax({
-      url: '/api/v1/ideas/' + $(idea).data('id') + '.json',
-      type: 'PUT',
-      data: {idea: { quality: quality } },
-      success: function() {
-        idea.find('#quality').html('<b>Quality:</b> ' + quality);
-      }
-    });
+  ideaService.edit($(idea).data('id'), data, function() {
+    idea.find('.quality').html('<b>Quality:</b> ' + quality);
   });
 
   return idea;
 }
 
 function addListenerForEdit(idea) {
-  idea.find('#edit-idea-' + idea.data('id')).on('click', function() {
-    idea.find('#edit-form-' + idea.data('id')).toggleClass('hidden')
+  idea.find('.edit-idea-' + idea.data('id')).on('click', function() {
+    idea.find('#edit-form-' + idea.data('id')).toggle('fast');
+  });
+}
+
+function addListenerToSubmitEdits(idea) {
+  idea.find('#submit-idea-' + idea.data('id')).on('click', function() {
+    event.preventDefault();
+
+    var $idea = $(this).closest('.idea');
+    var newTitle = $('#idea-title-' + $idea.attr('data-id')).val()
+    var newBody = $('#idea-body-' + $idea.attr('data-id')).val()
+
+    var formData = {
+      idea: {
+        title: newTitle,
+        body: newBody
+      }
+    }
+
+    ideaService.edit($idea.attr('data-id'), formData, function() {
+      $('#title-' + $idea.attr('data-id')).html(newTitle);
+      $('#body-' + $idea.attr('data-id')).html(newBody);
+
+      $('.edit-idea-' + $idea.attr('data-id')).click();
+    });
   });
 }
 
 function submitNewIdea() {
   $('#submit-idea').on('click', function() {
     event.preventDefault();
+
     var formData = {
       idea: {
         title: $('#idea-title').val(),
@@ -96,31 +127,67 @@ function submitNewIdea() {
       }
     }
 
-    $.ajax({
-      url: '/api/v1/ideas.json',
-      type: 'POST',
-      data: formData,
-      success: function(idea) {
-        appendIdeaToDom(idea);
+    ideaService.new(formData, function(err, idea) {
+      appendIdeaToDom(idea);
 
-        $('#idea-title').val('');
-        $('#idea-body').val('');
-      }
+      $('#idea-title').val('');
+      $('#idea-body').val('');
     });
   })
 }
 
-
 function deleteIdea() {
-  $('.ideas').delegate('#remove-idea', 'click', function() {
-    var $idea = $(this).closest('.idea');
+  $('.ideas').delegate('.remove-idea', 'click', function() {
+    var idea = $(this).closest('.idea');
+    var id   = idea.attr('data-id')
 
-    $.ajax({
-      url: '/api/v1/ideas/' + $idea.attr('data-id') + '.json',
-      type: 'DELETE',
-      success: function() {
-        $idea.remove();
-      }
+    ideaService.delete(id, function() {
+      idea.remove();
     });
   });
 }
+
+var ideaService = {
+  index: function(cb) {
+    $.ajax({
+      type: 'GET',
+      url: '/api/v1/ideas.json',
+      success: function(response) {
+        cb(response);
+      }
+    });
+  },
+
+  new: function(data, cb) {
+    $.ajax({
+      url: '/api/v1/ideas.json',
+      type: 'POST',
+      data: data,
+      success: function(idea) {
+        cb(null, idea);
+      }
+    });
+  },
+
+  edit: function(id, data, cb) {
+    $.ajax({
+      url: '/api/v1/ideas/' + id + '.json',
+      type: 'PUT',
+      data: data,
+      success: function() {
+        cb()
+      }
+    });
+  },
+
+  delete: function(data, cb) {
+    $.ajax({
+      url: '/api/v1/ideas/' + data + '.json',
+      type: 'DELETE',
+      success: function() {
+        cb();
+      }
+    });
+  }
+}
+
