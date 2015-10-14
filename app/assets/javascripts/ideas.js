b1267d3811557caa7c5c4a96f19f499ffb8ba9fa
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-10-14 13:34:04 -0600

    change id selector to class selector, remove unused CSS

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 function renderHeaderLinks() {
   return $('.links').append(
     '<img class="js-logo" src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" />'
-    + '<a href="#" class="not-a-link">Ideas</a><a id="link" href="#">All Ideas</a>'
+    + '<a href="#" class="not-a-link">Ideas</a>'
   )
 }
 
